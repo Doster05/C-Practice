3f7673e87f72c52cf30a019eae9cd18b3658f9a4
Doster0 <darrion.oster@gmail.com>
2026-05-01 20:12:09 -0500

    Learning Input

--- Trial1.c
+++ Trial1.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#define CUBIC_IN_PER_POUND 166
 
 int main(){
-    int one,two,three,four;
-    one = 1;
-    two = 2;
-    three = 3;
-    four = one + three;
-
+    int height, width, length, volume, weight;
+    
     //? Very odd that in C you have to do something like this for printing a variable 
-    printf("%d", four);
+    
+    printf("Enter Box Height: ");
+    scanf("%d", &height);
+    printf("Enter Box Width: ");
+    scanf("%d", &width);
+    printf("Enter Box Length: ");
+    scanf("%d", &length);
+
+    volume = height * width * length;
+    weight = (volume + CUBIC_IN_PER_POUND - 1) / CUBIC_IN_PER_POUND;
+
+    printf("Volume in cubic is: %d\n", volume);
+    printf("Weight in pounds is: %d\n", weight);
 
     return 0;
 }
